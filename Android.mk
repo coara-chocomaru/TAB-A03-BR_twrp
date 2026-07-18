5096be6183b5943ece3088d204c7032be7998393
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-18 14:35:24 +0900

    Remove LOCAL_SDK_VERSION assignment from Android.mk

--- Android.mk
+++ Android.mk
@@ -14,7 +14,6 @@ KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
-LOCAL_SDK_VERSION := 22
 JAVA_SDK_ENFORCEMENT_ERROR := false
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
