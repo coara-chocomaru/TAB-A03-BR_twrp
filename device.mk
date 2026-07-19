96ba4030c36f9d54b1b29dfea0fb42f313ae1b3e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-19 11:05:07 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,15 +5,3 @@ TARGET_IS_TABLET := true
 TARGET_NO_TELEPHONY := true
 TARGET_HAS_NO_RADIO := true
 LOCAL_PATH := device/sts/a03br
-
-PRODUCT_PACKAGES -= \
-    fb2png \
-    injecttwrp \
-    libtwrpmtp \
-    mtpd \
-    libopenaes \
-    ntfs-3g \
-    mkntfs \
-    su \
-    Superuser
-
