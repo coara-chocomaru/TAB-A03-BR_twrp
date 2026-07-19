5757ad222b5f477f184e75f3eaa5f2100676cd5e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-20 04:37:19 +0900

    Update omni_a03br.mk

--- omni_a03br.mk
+++ omni_a03br.mk
@@ -4,8 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/sts/a03br/device.mk)
