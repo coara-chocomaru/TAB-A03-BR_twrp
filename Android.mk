12b859a41f801955a64eaddff821b5ce320fab76
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-20 16:13:59 +0900

    Remove build flags for a03br device
    Remove TARGET_STRIP and related flags for a03br device.

--- Android.mk
+++ Android.mk
@@ -8,9 +8,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),a03br)
-TARGET_STRIP=true
-TARGET_CFLAGS+="-Os -ffunction-sections -fdata-sections -fno-unwind-tables"
-TARGET_LDFLAGS+="-Wl,--gc-sections -Wl,--strip-all"
 JAVA_SDK_ENFORCEMENT_ERROR := false
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
