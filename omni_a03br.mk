6ad5c80aea481f541ca16164902b53f6adae7b2c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-21 14:18:51 +0900

    Update omni_a03br.mk

--- omni_a03br.mk
+++ omni_a03br.mk
@@ -3,10 +3,9 @@
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/sts/a03br/device.mk)
 
 PRODUCT_DEVICE := a03br
