cb85c0187523733b3da148e00b1f85d181a2eb5a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-22 01:51:57 +0900

    Change source file from CPP to C and update flags

--- touch/Android.mk
+++ touch/Android.mk
@@ -1,16 +1,19 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := touchyfix
-LOCAL_SRC_FILES := touchyfix.cpp
+LOCAL_SRC_FILES := touchyfix.c
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
-LOCAL_CPPFLAGS += -Os -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti
-LOCAL_CFLAGS   += -Os -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti
-LOCAL_LDFLAGS  += -Wl,--gc-sections
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_CFLAGS += -Os -ffunction-sections -fdata-sections -fno-stack-protector
+LOCAL_LDFLAGS += -Wl,--gc-sections
+
+LOCAL_STATIC_LIBRARIES += libc
 
 include $(BUILD_EXECUTABLE)
