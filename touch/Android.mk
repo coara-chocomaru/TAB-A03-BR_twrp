99d1bf6f4f28e50b2ae2bdd8af8a2bea068ae1ea
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-22 02:10:11 +0900

    Update Android.mk

--- touch/Android.mk
+++ touch/Android.mk
@@ -8,12 +8,20 @@ LOCAL_SRC_FILES := touchyfix.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-LOCAL_CFLAGS += -Os -ffunction-sections -fdata-sections -fno-stack-protector
-LOCAL_LDFLAGS += -Wl,--gc-sections
+LOCAL_CFLAGS += \
+    -Os \
+    -g0 \
+    -ffunction-sections \
+    -fdata-sections \
+    -fno-stack-protector \
+    -fomit-frame-pointer
 
+LOCAL_LDFLAGS += \
+    -Wl,--gc-sections \
+    -Wl,-s
+LOCAL_STRIP_MODULE := true
 LOCAL_STATIC_LIBRARIES += libc
 
 include $(BUILD_EXECUTABLE)
